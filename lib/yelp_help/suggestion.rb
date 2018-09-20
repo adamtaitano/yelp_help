aa4ea903a63f6049d2f84396ed0f1d3b9d8c85d1
Adam Taitano <adam.taitano@gmail.com>
2018-09-19 20:20:38 -0700

    Suggestion hash data is formatted and ready to be printed for CLI. Scrape requires both a location and a search type.

--- lib/yelp_help/suggestion.rb
+++ lib/yelp_help/suggestion.rb
@@ -4,8 +4,8 @@ class YelpHelp::Suggestion
   #Initialize Suggestion using user input
   def initialize(location = [nil], search_type = "")
     #Set URL based on user input
-    location = ["portland", "or"]
-    search_type = "burgers"
+    location = ["il"]
+    search_type = "pancakes"
     @@url = "https://www.yelp.com/search?find_desc=#{search_type}&find_loc=#{location[0]}%2C+#{location[1]}&ns=1"
   end
   #Scrape yelp based on user input
@@ -14,35 +14,24 @@ class YelpHelp::Suggestion
     #go to yelp, enter url based on input data
     puts @@url
     doc = Nokogiri::HTML(open(@@url, 'User-Agent' => 'Ruby/2.4.1'))
-    suggestions_array = doc.search("li.regular-search-result")
-    #First suggestion element
-    suggestion_title = suggestions_array[0].search("a.biz-name span").text
-    suggestion_rating = suggestions_array[0].search("div.i-stars")[0].values[1]
-    suggestion_price_category = suggestions_array[0].search("span.price-range").text
-    # ##Need to iterate upon each element of category - find size of array and add each element to variable
-    suggestion_category_list = suggestions_array[0].search("span.category-str-list a[href]")[0].text
-    suggestion_address = suggestions_array[0].search("div address").text.strip
-    suggestion_neighborhood = suggestions_array[0].search("span.neighborhood-str-list").text.strip
-    suggestion_snippet = suggestions_array[0].search("p.snippet").text.strip
-    binding.pry
-    #extract suggestion properties
-    #instantiate a suggestion
+    data_array = doc.search("li.regular-search-result")
+    ##Iterate on each element of suggestions_array to create data hash objects
+    elements = data_array.each_with_index.map{|data, index| {
+      "number" => index + 1,
+      "title" => data.search("a.biz-name span").text,
+      "rating" => data.search("div.i-stars")[0].values[1],
+      "price" => data.search("span.price-range").text,
+      "category_list" => data.search("span.category-str-list a[href]").text.split(/(?=[A-Z])/),
+      "address" => data.search("div address").text.strip,
+      "neighborhood" => data.search("span.neighborhood-str-list").text.strip,
+      "snippet" => data.search("p.snippet").text.strip
+      }
+    }
+    @@all << elements
+    elements
   end
 
   def self.all
-    #Return a bunch of instances of Suggestion
-    suggestion_1 = self.new
-    suggestion_1.name = "Screen Door"
-    suggestion_1.rating = "5/5 Stars"
-    suggestion_1.type = "Restaurant"
-    @@all << suggestion_1
-
-    suggestion_2 = self.new
-    suggestion_2.name = "Masu Sushi"
-    suggestion_2.rating = "4.5/5 Stars"
-    suggestion_2.type = "Restaurant"
-    @@all << suggestion_2
-
     @@all
   end
 
